557eb649bd44ac040ea1cd52837d7c07ee94992e
svodnik <sashavodnik@gmail.com>
2018-09-01 12:32:35 -0700

    emptied trash

--- 08-events-jquery/starter-code/0-events-codealong/js/app.js
+++
@@ -1,5 +0,0 @@
-let mainHeading = document.createElement("h1");
-let headingText = document.createTextNode("Reminders");
-mainHeading.appendChild(headingText);
-document.querySelector('header').appendChild(mainHeading);
-
--- 09-advanced-jquery/starter-code/3-templating-codealong-1/js/main.js
+++
@@ -1,8 +0,0 @@
-const sessions = {
-  title: "Advances in Wasp Farming",
-  presenter: "Cassandra Fleming",
-  day: "Monday",
-  start: "12:30",
-  end: "2:00",
-  summary: "Recent advances and trends among wasp farmers"
-}
