6bb72f130989c6de2673a3d8b44f34a8a6d3144c
svodnik <sashavodnik@gmail.com>
2018-10-01 14:04:17 -0700

    added resources for Class 06

---
+++ 06-objects-json/solution-code/4-data-structure-exercise/app.js
@@ -0,0 +1,77 @@
+// Working with nested data structures
+//
+// The cakes variable below contains a JavaScript object.
+// Write a statement that logs the value of the type property of the second
+// element in the batter array to the console.
+// Hint: Remember to use dot syntax or square bracket notation to move down
+// one level, then repeat.
+
+'use strict';
+
+const cakes = {
+	"id": "0001",
+	"type": "donut",
+	"name": "Cake",
+	"ppu": 0.55,
+	"batters":
+		{
+			"batter":
+				[
+					{ "id": "1001", "type": "Regular" },
+					{ "id": "1002", "type": "Chocolate" },
+					{ "id": "1003", "type": "Blueberry" },
+					{ "id": "1004", "type": "Devil's Food" }
+				]
+		},
+	"topping":
+		[
+			{ "id": "5001", "type": "None" },
+			{ "id": "5002", "type": "Glazed" },
+			{ "id": "5005", "type": "Sugar" },
+			{ "id": "5007", "type": "Powdered Sugar" },
+			{ "id": "5006", "type": "Chocolate with Sprinkles" },
+			{ "id": "5003", "type": "Chocolate" },
+			{ "id": "5004", "type": "Maple" }
+		]
+};
+
+
+// PART A
+// 1. Write a statement that logs the value of the `batters` property to the
+//    console
+console.log(cakes.batters);
+
+// 2. Write a statement that logs the `batter` array to the console
+console.log(cakes.batters.batter);
+
+// 3. Write a statement that logs the second element in the `batter` array to
+//    the console.
+console.log(cakes.batters.batter[1]);
+
+// 4. Write a statement that logs the `type` property of the second element in
+//    `batter` array to the console.
+console.log(cakes.batters.batter[1].type);
+
+
+// PART B
+// 1. Write a statement that logs the id value of the fifth element in the
+//    `topping` array to the console.
+//    Hint: Remember to start at the top of the structure and use dot syntax or
+//    square bracket notation to move down one level at a time.
+console.log(cakes.topping[4].id);
+
+// BONUS
+// 1. Write code that logs the values of all the properties in the cakes object.
+//    Hint: Read up on the for/in iterator.
+//    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+for(let prop in cakes) {
+  console.log(cakes[prop]);
+}
+
+// 2. Write code that logs the _key names_ of all the properties in the cakes
+//    object.
+//    Hint: Read up on the for/in iterator (again).
+//    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+for(let prop in cakes) {
+  console.log(prop);
+}
